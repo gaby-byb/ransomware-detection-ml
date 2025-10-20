9d48dd40154b5049c96872eb46b6fcd34e3e6fed
gaby-byb <gabyatl@outlook.com>
2025-10-20 19:40:55 -0400

    new dataset group added

--- src/models.py
+++ src/models.py
@@ -2,9 +2,10 @@
 from scipy.stats import randint as sp_randint
 import pandas as pd
 from preprocessing import preprocess  
-from sklearn.model_selection import RandomizedSearchCV, train_test_split
+from sklearn.model_selection import RandomizedSearchCV, train_test_split, cross_val_score
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report
+from xgboost import XGBClassifier
 
 
 #load dataset
@@ -14,16 +15,23 @@ df = pd.read_csv("data/ransomware.csv")
 # function returns target and train data
 X, y, meta = preprocess(df)
 
+def train_model(model, df, target_col, test_size=0.3):
+    X = df.drop(target_col, axis=1)
+    y = df[target_col]
 
-#stratify handles class imbalance
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+    #stratify handles class imbalance
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+    model.fit(X_train, y_train)
 
+    
 """ --Hyperparameter Tuning--
 * n_estimators: the number of trees in the forest. Increasing it improves performance but increases cost
 * max_depth: max depth of each tree. Reduces overfitting
 """
 param_dist = {'n_estimators': sp_randint(50,500),
               'max_depth': sp_randint(1,20)}
+
+#----- RANDOM FOREST -----#
 
 #instance of Random forest model 
 rf = RandomForestClassifier(random_state=42)
@@ -51,5 +59,28 @@ y_pred = rf_best.predict(X_test)
 #Predictions on test data
 accuracy = accuracy_score(y_test, y_pred)
 classification_rep = classification_report(y_test, y_pred)
-print(f"Accuracy: {accuracy:.2f}")
+print(f"Random Forest Accuracy: {accuracy:.2f}")
 print("\nClassification Report:\n", classification_rep)
+
+
+#----- XGBoost -----#
+xgb = XGBClassifier(
+    n_estimators=200,
+    learning_rate=0.1,
+    max_depth=6,
+    subsample=0.8,
+    colsample_bytree=0.8,
+    random_state=42,
+    use_label_encoder=False,
+    eval_metric='logloss'  # suppresses warning
+)
+xgb.fit(X_train, y_train)
+
+y_pred = xgb.predict(X_test)
+cross_val_score(xgb, X, y, cv=5, scoring='f1')
+#evaluate
+print("XGBoost Metrics", classification_report(y_test, y_pred))
+
+# ---- SVM ----
+
+
